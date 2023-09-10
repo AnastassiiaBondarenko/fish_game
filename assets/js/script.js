b0de104d5c3ad0777d367023386a306129810bcc
Анастасия Бондаренко <anastasiabondarenko@Anastasias-MacBook-Pro.local>
2023-09-10 19:33:14 +0100

    Add footer and style it. clean up my code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
+// This event listener ensures that the DOM is fully loaded before executing the script
 document.addEventListener("DOMContentLoaded", () => {
+    // Get references to various DOM elements
     const rulesSection = document.getElementById("rules");
     const rulesLink = document.getElementById("rules-link");
     const closeButton = document.getElementById("rules-close");
@@ -22,9 +24,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const catchSound = document.getElementById("catchSound");
     const homeIcon = document.getElementById("home-link");
     const main = document.getElementById("main");
+    const fishElements = document.querySelectorAll(".fish");
+    const scoreElement = document.getElementById("score");
+    const toggleButton = document.getElementById("toggle-footer");
+    const footer = document.getElementById("hidden-footer");
+    const closeFooterButton = document.getElementById("close-footer");
 
-
-    // open and close rules section 
+    // Open and close rules section
+    /**
+     * This function opens the rules section and handles game pause logic if the game has started.
+     */
     function openRulesSection() {
         rulesSection.classList.add("show");
         if (gameStarted) {
@@ -35,6 +44,9 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    /**
+     * This function closes the rules section and handles game resume logic if the game has started.
+     */
     function closeRulesSection() {
         rulesSection.classList.remove("show");
         if (gameStarted) {
@@ -46,12 +58,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     rulesLink.addEventListener("click", openRulesSection);
-
     closeButton.addEventListener("click", closeRulesSection);
 
-    // sound on and off 
+    // Sound on and off
     let soundOn = false;
 
+    /**
+     * This function toggles the game sound on and off.
+     */
     function toggleSound() {
         if (soundOn) {
             audioElement.pause();
@@ -66,6 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
         soundOn = !soundOn;
     }
 
+    // Initialize sound settings
     audioElement.pause();
     audioElement.currentTime = 0;
     soundIcon.src = "assets/images/icon-mute.png";
@@ -73,20 +88,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
     soundControl.addEventListener("click", toggleSound);
 
-
-
-    // start game and pause
+    // Start game and pause
     let gameStarted = false;
     let timerInterval;
     let timeLeft = 60;
     let gameEnded = false;
 
-
     // Function to start the game
-
-
+    /**
+     * This function starts the game, hides the main title and play block,
+     * displays the game section, sets up timers, and handles game-related UI changes.
+     */
     function startGame() {
-
         if (gameEnded) {
             return;
         }
@@ -116,16 +129,17 @@ document.addEventListener("DOMContentLoaded", () => {
         pauseButton.addEventListener("click", pauseGame);
     }
 
-    // timer 
+    // Timer
+    /**
+     * This function updates the timer display with the remaining time in minutes and seconds.
+     */
     function updateTimerDisplay(seconds) {
         const minutes = Math.floor(seconds / 60);
         const remainingSeconds = seconds % 60;
         timer.textContent = `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
     }
 
-
-
-    //  pause
+    // Pause
     pauseButton.style.display = "none";
     resumeButton.style.display = "none";
     confirmationModal.style.display = "none";
@@ -134,24 +148,26 @@ document.addEventListener("DOMContentLoaded", () => {
         confirmationModal.style.display = "block";
     });
 
+    /**
+     * This function hides the pause confirmation modal.
+     */
     function hideModal() {
         confirmationModal.style.display = "none";
     }
-
 
     continueButton.addEventListener("click", () => {
         hideModal();
         resumeGame();
     });
 
-
     homeButton.addEventListener("click", () => {
         hideModal();
-
         window.location.href = window.location.origin;
     });
 
-
+    /**
+     * This function pauses the game, clears the timer, and handles fish animation pause.
+     */
     function pauseGame() {
         clearInterval(timerInterval);
         pauseButton.style.display = "none";
@@ -164,9 +180,10 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-
-
-    // resume 
+    // Resume
+    /**
+     * This function resumes the game, restarts the timer, and resumes fish animations.
+     */
     function resumeGame() {
         timerInterval = setInterval(function () {
             timeLeft--;
@@ -185,25 +202,29 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    //  end the game
+    // End the game
     homeIcon.addEventListener("click", () => {
         if (gameStarted) {
-
             hideModal();
             endGame();
         }
-
     });
 
+    /**
+     * This function ends the game, clears the timer, displays the final score, and handles game end logic.
+     */
     function endGame() {
         clearInterval(timerInterval);
         pauseButton.style.display = "none";
         finalScore.textContent = score;
         endModal.style.display = "block";
         gameEnded = true;
-        pauseGame()
+        pauseGame();
     }
 
+    /**
+     * This function closes the end game modal and redirects the player to the main page.
+     */
     function closeModalAndRedirect() {
         endModal.style.display = "none";
         window.location.href = window.location.origin;
@@ -219,64 +240,53 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-
-
-    startButton.addEventListener("click", function () {
-        if (!gameStarted) {
-            startGame();
-            gameStarted = true;
-        }
-    });
-
-
-    // game area 
-    const fishElements = document.querySelectorAll(".fish");
-    const scoreElement = document.getElementById("score");
+    // Game area
 
     let score = 0;
 
+    /**
+     * This function plays the catch sound when a fish is caught.
+     */
     function playCatchSound() {
         catchSound.currentTime = 0;
         catchSound.play();
     }
 
-
+    /**
+     * This function updates the player's score and displays it on the screen.
+     */
     function updateScore() {
         score++;
         scoreElement.textContent = score;
     }
 
+    /**
+     * This function moves a fish element to a random position within the game area.
+     */
     function moveFishToRandomPosition(fish) {
-        const fishWidth = fish.offsetWidth;
-        const fishHeight = fish.offsetHeight;
-        const maxX = window.innerWidth - fishWidth;
-        const maxY = window.innerHeight - fishHeight;
-        const newX = Math.random() * maxX;
-        const newY = Math.random() * maxY;
-
+        let fishWidth = fish.offsetWidth;
+        let fishHeight = fish.offsetHeight;
+        let maxX = window.innerWidth - fishWidth;
+        let maxY = window.innerHeight - fishHeight;
+        let newX = Math.random() * maxX;
+        let newY = Math.random() * maxY;
 
         fish.style.transform = `translate(${newX}px, ${newY}px)`;
     }
 
     fishElements.forEach((fish) => {
         fish.addEventListener("click", () => {
-
             playCatchSound();
             updateScore();
             fish.style.display = "none";
             setTimeout(() => {
                 fish.style.display = "block";
                 moveFishToRandomPosition(fish);
-
             });
-        });;
+        });
 
         moveFishToRandomPosition(fish);
     });
-
-
-
-
 
     startButton.addEventListener("click", function () {
         if (!gameStarted) {
@@ -285,5 +295,35 @@ document.addEventListener("DOMContentLoaded", () => {
             gameStarted = true;
         }
     });
+
+    // footer 
+    /**
+     * This function closes the footer and makes it unvisible.
+     */
+    function hideFooter() {
+        footer.style.bottom = "-300px";
+    }
+
+
+    toggleButton.addEventListener("click", () => {
+        if (footer.style.bottom === "-300px" || !footer.style.bottom) {
+            footer.style.bottom = "0";
+        } else {
+            hideFooter();
+        }
+    });
+
+    // close the footer when clicked inside
+    footer.addEventListener("click", (event) => {
+        if (event.target === footer) {
+            hideFooter();
+        }
+    });
+
+
+    closeFooterButton.addEventListener("click", () => {
+        hideFooter();
+    });
+
 
 });
